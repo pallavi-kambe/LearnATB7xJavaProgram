b1179dc7595e6431612aea7add3856506eeadb74
Pallavi <pallavikambe7@gmail.com>
2024-07-07 14:10:37 +0530

    commit

---
+++ src/ex_07072024/Lab003.java
@@ -0,0 +1,20 @@
+package ex_07072024;
+
+import java.sql.SQLOutput;
+
+public class Lab003
+{
+    public static void main(String[] args)
+    {
+        System.out.println("ABCD");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(2*2);
+
+
+
+        }
+    }
+
+
