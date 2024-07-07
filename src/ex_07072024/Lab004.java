ef5c7202fdea31c940f8819f9f565be78302a5b1
Pallavi <pallavikambe7@gmail.com>
2024-07-07 14:17:11 +0530

    commit

---
+++ src/ex_07072024/Lab004.java
@@ -0,0 +1,12 @@
+package ex_07072024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("I am Pallavi");
+        System.out.println("");
+        System.out.println("");
+        System.out.print("Hello world");
+        System.out.print("I am Pallavi");
+    }
+}
