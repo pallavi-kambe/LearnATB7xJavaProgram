41befee51316fb6ee99f942128d40edf37b523a8
Pallavi <pallavikambe7@gmail.com>
2024-07-13 15:39:17 +0530

    commit

---
+++ src/ex_13072024/lab011.java
@@ -0,0 +1,19 @@
+package ex_13072024;
+
+public class lab011 {
+    public static void main(String[] args) {
+        System.out.println("9x1=9");
+        System.out.println("9x2=18");
+        System.out.println("9x10=90");
+        System.out.println();
+
+        int a = 9*1;
+        System.out.println("9*1=" +a);
+
+        int b=9*2;
+        System.out.println(b);
+
+        int c= 9*3;
+        System.out.println(c);
+    }
+}
---
+++ src/ex_13072024/lab06.java
@@ -0,0 +1,15 @@
+package ex_13072024;
+
+public class lab06 {
+    public static void main(String[] args) {
+        byte b  = 10;
+        System.out.println(b);
+        // JVM -> brother who will execute the main method
+        // 10 - Decimal
+        // Machine(0,1) -> 00001010 - 8 bits = 1 Byte
+        long l = 3844000000000000l;
+        System.out.println(l);
+
+    }
+}
+
