690a0bd7675a85a58e3d9117960d26283ae254d9
Pallavi <pallavikambe7@gmail.com>
2024-07-13 14:22:34 +0530

    commit

---
+++ src/ex_13072024/Lab03.java
@@ -0,0 +1,24 @@
+package ex_13072024;
+
+import java.sql.SQLOutput;
+
+public class Lab03 {
+    public static void main(String[] args) {
+        //concatenation
+        //Append
+        String s = "Pallavi";
+        int a = 10;
+        int y = 20;
+        /* Concat+Append */
+        System.out.println(a+y+s);
+        System.out.println(s+a+y); //Append+Concat
+        System.out.println(a+s+y);
+        System.out.println(a+s);
+        System.out.println(s+a);
+        System.out.println(a+y);
+        System.out.println(y+s);
+        System.out.println(s+y);
+        System.out.println(s+s);
+        System.out.println(a+a);
+    }
+}
