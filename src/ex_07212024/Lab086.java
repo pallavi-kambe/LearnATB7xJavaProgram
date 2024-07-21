8a468eba7c2c51cf321b1e59248a09b8ed59dffc
Pallavi <pallavikambe7@gmail.com>
2024-07-21 21:35:34 +0530

    21072024

---
+++ src/ex_07212024/Lab086.java
@@ -0,0 +1,18 @@
+package ex_07212024;
+
+public class Lab086 {
+    public static void main(String[] args) {
+        int i = 1;
+//        while (i<10){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<10);{
+            System.out.println(i);
+        }
+    }
+}
