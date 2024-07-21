3612914be399dec273f4c3aa795dbdf3a8bf9aa1
Pallavi <pallavikambe7@gmail.com>
2024-07-21 21:26:20 +0530

    21072024

--- src/ex_07212024/Lab085.java
+++ src/ex_07212024/Lab085.java
@@ -2,7 +2,31 @@ package ex_07212024;
 
 public class Lab085 {
     public static void main(String[] args) {
+        // while, for -?
+        // do while - while - 1 change - minor change
+
+        // While -> int -> condition -> body -> increment / decrement
+
+        // Do While
+        // int -> body -> condition -> incre / drement
+
+        //do {
+            //int i=0;
+            //System.out.println(i);
+            //i++;
+        //}
+        //while(i<=5);
+
+        int a = 0;
+        do {
+            System.out.println(a);
+            a++;
+        }while(a < 10);
+
+
+
 
 
     }
 }
+
